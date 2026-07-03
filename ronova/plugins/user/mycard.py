00e752a078f479449ab7d1e6342d6aa38ed9895a
Breeze <akshadsatpute1989@gmail.com>
2026-07-03 14:56:15 +0530

    fixes

--- ronova/plugins/user/mycard.py
+++ ronova/plugins/user/mycard.py
@@ -8,50 +8,52 @@ from pyrogram.types import Message, ReplyParameters
 from config import PREFIXES, BOT
 
 
-import os
-
-async def get_card():
+# ✅ Generate cards
+async def get_card(uid: str = "1817389136"):
     os.makedirs("gi_downloads", exist_ok=True)
 
-    async with encbanner2.ENC(uid="1817389136") as encard:
+    async with encbanner2.ENC(uid=uid) as encard:
         result = await encard.creat(2)
 
     for char in result.card:
-        name = (char.name or "").strip()
-        if not name:
-            name = str(char.id)
-
-        safe_name = name
+        name = (char.name or "").strip() or str(char.id)
+        safe_name = name.replace("/", "_")
 
         img = char.card
-        img = img.resize((800, int(img.height * (800 / img.width))), Image.LANCZOS)
-        img.save(f"gi_downloads/{char.id}_{safe_name}.png", format="PNG", optimize=True)
+        img = img.resize(
+            (800, int(img.height * (800 / img.width))),
+            Image.LANCZOS
+        )
 
+        img.save(f"gi_downloads/{char.id}_{safe_name}.png", "PNG", optimize=True)
 
 @Client.on_message(filters.command("mycard", prefixes=PREFIXES))
-async def mygicard(c:Client, m:Message):
+async def mygicard(c: Client, m: Message):
 
-    msg = await m.reply("please wait...")
+    msg = await m.reply("Generating your cards...")
 
-    if not os.path.exists(f".enka_py"):
-        await msg.edit("Downloading assests...")
-        from enkanetwork import EnkaNetworkAPI
-        enka_update = EnkaNetworkAPI()
-        async with enka_update:
-            await enka_update.update_assets()
-    
-    if not os.path.exists(f"gi_downloads"):
-        await msg.edit("fetching data...")
+    try:
         await get_card()
 
-    results = await c.get_inline_bot_results(bot=BOT, query=f"genshin_card")
+    except Exception as e:
+        await msg.edit(f"Error:\n{e}")
+        return
 
-    await c.send_inline_bot_result(
-        chat_id=m.chat.id,
-        query_id=results.query_id,
-        result_id=results.results[0].id,
-        reply_parameters=ReplyParameters(message_id=m.id)
-    )
+    try:
+        results = await c.get_inline_bot_results(
+            bot=BOT,
+            query="genshin_card"
+        )
+
+        await c.send_inline_bot_result(
+            chat_id=m.chat.id,
+            query_id=results.query_id,
+            result_id=results.results[0].id,
+            reply_parameters=ReplyParameters(message_id=m.id)
+        )
+
+    except Exception as e:
+        await msg.edit(f"" Inline error:\n{e}")
+        return
+
     await msg.delete()
-    
-    
