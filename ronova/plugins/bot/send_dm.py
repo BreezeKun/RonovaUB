59b006cecbf7c6d9c31ce08e32c6e922ebc60709
Breeze <akshadsatpute1989@gmail.com>
2026-07-08 11:44:24 +0530

    now you can talk someone with bots dm by replying to their message target should send a message in bot tho

---
+++ ronova/plugins/bot/send_dm.py
@@ -0,0 +1,13 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from config import ADMIN_ID
+
+@Client.on_message(filters.private)
+async def send_dm(c: Client, m: Message):
+    if m.from_user.id != ADMIN_ID[0]:
+        await c.forward_messages(ADMIN_ID[0], m.chat.id, m.id)
+    else:
+        if m.from_user.id == ADMIN_ID[0]:
+            if m.reply_to_message:
+                r = m.reply_to_message.forward_origin.sender_user
+                await c.send_message(r.id, m.text)
