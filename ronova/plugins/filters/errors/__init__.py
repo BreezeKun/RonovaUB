df0822393229082b7c1a3c489d536a511a19c83c
Breeze <akshadsatpute1989@gmail.com>
2026-07-11 18:22:33 +0530

    added custom error

---
+++ ronova/plugins/filters/errors/__init__.py
@@ -0,0 +1,3 @@
+from .no_data import NoData
+
+__all__ : list[str] = ["NoData"]
--- ronova/plugins/filters/guest_starts.py
+++ ronova/plugins/filters/guest_starts.py
@@ -1,9 +1,15 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-def starts(prefix:str):
-    async def func(flt, c:Client, m:Message):
+from .errors import NoData
+
+def starts(prefix: str | None = None):
+    if prefix is None:
+        raise NoData("pass a prefix in starts(prefix:str = '')")
+
+    async def func(flt, c: Client, m: Message):
         if not m.text:
             return False
         return m.text.startswith(prefix)
+
     return filters.create(func, name="StartsFilter")
