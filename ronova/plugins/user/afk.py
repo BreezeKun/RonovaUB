d866258001ccd4122c032b75f162547d4fe4cff4
Breeze <akshadsatpute1989@gmail.com>
2026-07-21 17:41:03 +0530

    can get expired file

--- ronova/plugins/user/afk.py
+++ ronova/plugins/user/afk.py
@@ -1,15 +1,13 @@
 import time
-import asyncio
 
 from pyrogram import Client, filters
-from pyrogram.enums import ChatType, MessageMediaType, MessageEntityType
-from pyrogram.errors import FloodWait
-from pyrogram.types import Message, ReplyParameters
+from pyrogram.types import Message
 
 from config import ADMIN_ID, PREFIXES
 from ..shared import AFK_DATA
 from ..utilities import format_time, refresh_data, send, extract_media
 from ..filters import to_me
+from decorators import on_expired
 
 @Client.on_message(filters.command(["afk", "brb"], prefixes=PREFIXES) & filters.user(ADMIN_ID), group= 2)
 async def afk(c:Client, m:Message):
@@ -22,10 +20,12 @@ async def afk(c:Client, m:Message):
 
     if m.reply_to_message:
         AFK_DATA.file_id, AFK_DATA.file_type = extract_media(m.reply_to_message)
+        AFK_DATA.media_from_chat, AFK_DATA.message_media_id = m.chat.id, m.reply_to_message.id
 
     await m.delete()
 
 @Client.on_message(filters.me, group= 1)
+@on_expired
 async def rem_afk(c:Client, m:Message):
     if AFK_DATA.status:
         duration = format_time(int(time.time() - AFK_DATA.afk_time))
@@ -41,6 +41,7 @@ async def rem_afk(c:Client, m:Message):
     & ~filters.bot,
     group=0
 )
+@on_expired
 async def on_afk(c: Client, m: Message):
 
     if not AFK_DATA.status:
