125d3bbfb1825333d7858a88bceac40ddb011fb7
BreezeKun <akshadsatpute1989@gmail.com>
2026-08-01 18:50:55 +0530

    minor fix

--- ronova/plugins/decorators/get_decorator.py
+++ ronova/plugins/decorators/get_decorator.py
@@ -1,9 +1,11 @@
 from functools import wraps
 from ..shared import HELP_STORAGE
 
-def get_string(func, command:str):
-    HELP_STORAGE.data.update({command:func.__doc__})
-    @wraps(func)
-    async def wrapper(*args, **kwargs):
-        return await func(*args, **kwargs)
-    return wrapper
+def get_string(command:str):
+    def decorator(func):
+        HELP_STORAGE.data.update({command:func.__doc__})
+        @wraps(func)
+        async def wrapper(*args, **kwargs):
+            return await func(*args, **kwargs)
+        return wrapper
+    return decorator
