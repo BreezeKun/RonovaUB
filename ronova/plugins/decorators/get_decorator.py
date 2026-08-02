808d79e56d62106d64324cac55b656172ebc0aae
BreezeKun <akshadsatpute1989@gmail.com>
2026-08-02 15:11:30 +0530

    adding data if alr exists

--- ronova/plugins/decorators/get_decorator.py
+++ ronova/plugins/decorators/get_decorator.py
@@ -3,7 +3,10 @@ from ..shared import HELP_STORAGE
 
 def get_string(command:str):
     def decorator(func):
-        HELP_STORAGE.data.update({command:func.__doc__})
+        if command in HELP_STORAGE.data:
+            HELP_STORAGE.data[command] += f"\n{func.__doc__}"
+        else:
+            HELP_STORAGE.data.update({command:func.__doc__})
         @wraps(func)
         async def wrapper(*args, **kwargs):
             return await func(*args, **kwargs)
