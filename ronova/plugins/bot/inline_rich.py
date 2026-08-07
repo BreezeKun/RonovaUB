ac1a7dd392229e149ef7ae32ae8a30d6a7970554
BreezeKun <akshadsatpute1989@gmail.com>
2026-08-07 14:46:06 +0530

    decorator for rich message

--- ronova/plugins/bot/inline_rich.py
+++ ronova/plugins/bot/inline_rich.py
@@ -4,9 +4,16 @@ from pyrogram.types import (InputRichMessage, InlineQuery,
 from richparser import parse
 
 from config import ADMIN_ID
+from ..decorators import get_string
 
 @Client.on_inline_query(filters.regex(r"rich (.+)") & filters.user(ADMIN_ID))
+@get_string("rich:message")
 async def inline_ani(c: Client, q: InlineQuery):
+    """
+    To send a rich message
+    Usage: @botname rich [message]
+    Note parsing is done by library richparser eg _h1:text => <h1>text</h1>
+    """
     text = parse(q.matches[0].group(1))
 
     await q.answer([
