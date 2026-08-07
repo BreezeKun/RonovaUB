2aa93d8efeb38377f681d34ecd5ca8e8682332aa
BreezeKun <akshadsatpute1989@gmail.com>
2026-08-07 11:09:58 +0530

    fixes

--- ronova/plugins/user/xox.py
+++ ronova/plugins/user/xox.py
@@ -1,13 +1,11 @@
 from pyrogram import Client, filters
-from pyrogram.types import Message
+from pyrogram.types import Message, ReplyParameters
 
 from config import PREFIXES, ADMIN_ID, BOT
 
 
 @Client.on_message(filters.command("xox", prefixes=PREFIXES) & filters.user(ADMIN_ID))
 async def xox(c: Client, m: Message):
-    if not m.reply_to_message or not m.reply_to_message.from_user:
-        return await m.reply_text("Reply to a user's message to challenge them.")
 
     user = m.from_user.id
     target = m.reply_to_message.from_user.id
@@ -21,5 +19,6 @@ async def xox(c: Client, m: Message):
         chat_id=m.chat.id,
         query_id=results.query_id,
         result_id=results.results[0].id,
-        reply_to_message_id=m.id
+        reply_parameters=ReplyParameters(message_id=m.reply_to_message.id)
     )
+    await m.delete()
