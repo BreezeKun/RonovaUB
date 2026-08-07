46079d955ead8fb088e6cd4910f870d239a89578
BreezeKun <akshadsatpute1989@gmail.com>
2026-08-07 11:04:18 +0530

    fixes

--- ronova/plugins/bot/inline_xox.py
+++ ronova/plugins/bot/inline_xox.py
@@ -1,26 +1,29 @@
 from pyrogram import Client, filters
 from pyrogram.enums import ButtonStyle
 from pyrogram.types import (
-    InlineQuery, InlineKeyboardMarkup, InlineKeyboardButton, InlineQueryResultArticle, InputTextMessageContent,
-    CallbackQuery
+    InlineQuery, InlineKeyboardMarkup, InlineKeyboardButton,
+    InlineQueryResultArticle, InputTextMessageContent, CallbackQuery
 )
 
 from ..shared import XOX_DATA
 
+
 def gen_board():
     if XOX_DATA.status == False:
+        XOX_DATA.board = []
         row = []
 
         while True:
-            row.append("_")
+            row.append(" ")
             if len(row) == 3:
                 XOX_DATA.board.append(row)
                 row = []
-                
+
             if len(XOX_DATA.board) == 3:
                 break
 
-def check_winner(board = XOX_DATA.board):
+
+def check_winner(board=XOX_DATA.board):
     for row in board:
         if row[0] == row[1] == row[2] != ' ':
             return row[0]
@@ -36,50 +39,55 @@ def check_winner(board = XOX_DATA.board):
 
     return None
 
+
 def gen_keyboard() -> list[list[InlineKeyboardButton]]:
-    temp_row = []
     keyboard = []
+
     for i in XOX_DATA.board:
+        temp_row = []
+
         for j in i:
             if j == ' ':
-                temp_row.append(InlineKeyboardButton(" ", callback_data=""))
+                temp_row.append(InlineKeyboardButton(" ", callback_data="empty"))
             elif j == "x":
-                temp_row.append(InlineKeyboardButton("X", callback_data="", style=ButtonStyle.DANGER))
+                temp_row.append(InlineKeyboardButton("X", callback_data="x", style=ButtonStyle.DANGER))
             elif j == "o":
-                temp_row.append(InlineKeyboardButton("O", callback_data="", style=ButtonStyle.DANGER))
+                temp_row.append(InlineKeyboardButton("O", callback_data="o", style=ButtonStyle.DANGER))
+
         keyboard.append(temp_row)
-        temp_row = []
+
     return keyboard
 
-@Client.on_inline_query(filters.regex("^xox_"))
-async def inline_xox(c:Client, q:InlineQuery):
-    data = q.query.split("_")
 
-    print(data)
+@Client.on_inline_query(filters.regex("^xox_"))
+async def inline_xox(c: Client, q: InlineQuery):
+    data = q.query.split("_")
 
     user = data[1]
     target = data[2]
 
     keyboard = InlineKeyboardMarkup([[
-            InlineKeyboardButton("accept", callback_data=f"accept_{user}_{target}", style=ButtonStyle.SUCCESS),
-            InlineKeyboardButton("refuse", callback_data=f"refuse_{user}_{target}", style=ButtonStyle.DANGER)
-        ]])
+        InlineKeyboardButton("accept", callback_data=f"accept_{user}_{target}", style=ButtonStyle.SUCCESS),
+        InlineKeyboardButton("refuse", callback_data=f"refuse_{user}_{target}", style=ButtonStyle.DANGER)
+    ]])
 
     await q.answer([
-            InlineQueryResultArticle(
-                title=f"xox game",
-                input_message_content=InputTextMessageContent(
-                    message_text="test"
-                ),reply_markup=keyboard
-                )
-        ], cache_time=0)
+        InlineQueryResultArticle(
+            title="xox game",
+            input_message_content=InputTextMessageContent(
+                message_text="test"
+            ),
+            reply_markup=keyboard
+        )
+    ], cache_time=0)
+
 
 @Client.on_callback_query(filters.regex("^accept_|refuse_"))
-async def decision(c:Client, cq:CallbackQuery):
+async def decision(c: Client, cq: CallbackQuery):
     data = cq.data.split("_")
     choice = data[0]
-    user = data[1]
-    target = data[2]
+    user = int(data[1])
+    target = int(data[2])
 
     if cq.from_user.id != target:
         return await cq.answer("Nope", show_alert=True)
@@ -90,12 +98,12 @@ async def decision(c:Client, cq:CallbackQuery):
             text="refused"
         )
     else:
-        XOX_DATA.status = True
         gen_board()
+        XOX_DATA.status = True
         keyboard = gen_keyboard()
 
         await c.edit_inline_text(
             inline_message_id=cq.inline_message_id,
-            text= "hmm",
+            text="hmm",
             reply_markup=InlineKeyboardMarkup(keyboard)
         )
