6a689a300e81fb3cf32f7c27c1976b65cd87f887
BreezeKun <akshadsatpute1989@gmail.com>
2026-08-20 20:08:57 +0530

    a bit better

--- ronova/__main__.py
+++ ronova/__main__.py
@@ -22,46 +22,46 @@ def close_db():
 async def startup():
     zn = pytz.timezone("Asia/Kolkata")
     time = datetime.now(zn)
+    await bot.start()
+    await ub.start()
     await bot.send_message(-1003792991167,f"started {time}")
+    from .plugins.database import sudo_methods
+    sudo_methods.build_cache()
+    
+    asyncio.create_task(startServer())
+    
+    print("Bot and UB started!")
+    
+    await idle()
+    
 
 async def on_stop():
     await bot.send_message(-1003792991167,"stopped")
+    try:
+        if ub.is_connected:
+            await ub.stop()
+    except Exception:
+        pass
+
+    try:
+        if bot.is_connected:
+            await bot.stop()
+    except Exception:
+        pass
+
+    try:
+        await close_session()
+    except Exception:
+        pass
+
 
 uvloop.install()
 async def main():
     try:
-        await bot.start()
-        await ub.start()
-        
         await startup()
-
-        from .plugins.database import sudo_methods
-        sudo_methods.build_cache()
-
-        asyncio.create_task(startServer())
-
-        print("Bot and UB started!")
-
-        await idle()
-
+        
     finally:
         await on_stop()
-        try:
-            if ub.is_connected:
-                await ub.stop()
-        except Exception:
-            pass
-
-        try:
-            if bot.is_connected:
-                await bot.stop()
-        except Exception:
-            pass
-
-        try:
-            await close_session()
-        except Exception:
-            pass
 
 
 if __name__ == "__main__":
