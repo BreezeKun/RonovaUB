d437c4aa787b7683553ef5a9ce58a2ebe75f9b0b
BreezeKun <akshadsatpute1989@gmail.com>
2026-08-21 17:25:19 +0530

    testing

--- ronova/plugins/user/play.py
+++ ronova/plugins/user/play.py
@@ -3,6 +3,7 @@ from pyrogram.types import Message
 
 from config import ADMIN_ID, PREFIXES
 from ..decorators import get_string
+from ..utilities import MUSIC_PLAYER
 
 @Client.on_message(filters.command("play", prefixes=PREFIXES) & filters.user(ADMIN_ID))
 @get_string("play")
@@ -15,6 +16,5 @@ async def play(c:Client, m:Message):
         return await m.reply("usage: play [music]")
 
     music =" ".join(m.command[1:])
-
-    
-    
+    MUSIC_PLAYER.add_queue(music)
+    await m.reply(MUSIC_PLAYER.play_music())
