dfc104297a938c4c86facf7f822920bacfaa5199
BreezeKun <akshadsatpute1989@gmail.com>
2026-08-21 17:25:32 +0530

    testing

--- ronova/plugins/utilities/get_music.py
+++ ronova/plugins/utilities/get_music.py
@@ -4,23 +4,40 @@ import yt_dlp
 
 class GetMusic:
     queue = []
+    status = False
 
-    def get_from_yt(self, music:str):
-        yt = YTMusic()
+    def get_from_yt(self):
+        if self.status:
+            yt = YTMusic()
 
-        results = yt.search(music, filter="songs")
+            results = yt.search(self.queue[0], filter="songs")
 
-        if not results:
-            return print("No data found")
+            if not results:
+                return print("No data found")
 
-        song = results[0]
+            song = results[0]
 
-        url = f"https://www.youtube.com/watch?v={song['videoId']}"
+            url = f"https://www.youtube.com/watch?v={song['videoId']}"
 
-        ydl_opts = {
-            "format": "ba[abr<=80]/ba",
-            "outtmpl": "%(title)s.%(ext)s",
-        }
+            ydl_opts = {
+                "format": "ba[abr<=80]/ba",
+                "outtmpl": "%(title)s.%(ext)s",
+            }
 
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([url])
+
+            self.queue.pop(0)
+
+            return song
+
+    def add_queue(self, music:str):
+        if music not in self.queue:
+            self.queue.append(music)
+
+    def play_music(self):
+        if not self.status:
+            self.status = True
+            return self.get_from_yt()
+
+MUSIC_PLAYER = GetMusic()
