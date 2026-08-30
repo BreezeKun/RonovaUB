478ce7b995764d8f662553f6892c841083784e59
BREEZE <157225495+BreezeKun@users.noreply.github.com>
2026-08-30 15:40:59 +0530

    Fix syntax error in card.py

--- ronova/plugins/user/genshin_impact/card.py
+++ ronova/plugins/user/genshin_impact/card.py
@@ -41,7 +41,7 @@ async def generate_character_card(name: str):
             "Odette": "https://files.catbox.moe/w1tgs5.jpg",
             "Mavuika": "https://files.catbox.moe/fojcvc.jpg",
             "Escoffier": "https://files.catbox.moe/i29ary.jpg",
-        }
+        },akasha=True
     ).generate()
 
     # Save response_data to JSON
