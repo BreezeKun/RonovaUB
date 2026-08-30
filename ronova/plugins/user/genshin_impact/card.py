2c7b093242859c14eba527497c6a2f93d7ab585b
BREEZE <157225495+BreezeKun@users.noreply.github.com>
2026-08-30 14:59:25 +0530

    Remove 'akasha' parameter from card generation
    Removed 'akasha' parameter from the card generation function.

--- ronova/plugins/user/genshin_impact/card.py
+++ ronova/plugins/user/genshin_impact/card.py
@@ -41,8 +41,7 @@ async def generate_character_card(name: str):
             "Odette": "https://files.catbox.moe/w1tgs5.jpg",
             "Mavuika": "https://files.catbox.moe/fojcvc.jpg",
             "Escoffier": "https://files.catbox.moe/i29ary.jpg",
-        },
-        akasha=False
+        }
     ).generate()
 
     # Save response_data to JSON
