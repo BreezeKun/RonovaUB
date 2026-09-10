28562d36afcdc2b1a7e341c55d1a99184dc4316b
BreezeKun <akshadsatpute1989@gmail.com>
2026-09-10 19:04:37 +0530

    fixed init

--- ronova/plugins/__init__.py
+++ ronova/plugins/__init__.py
@@ -1,3 +1,3 @@
-from . import bot, filters, user, utilities, shared
+from . import bot, filters, user, utilities, shared, database, decorators, premium
 
-__all__: list[str] = ['bot', 'filters', 'user', 'utilities', 'shared']
+__all__: list[str] = ['bot', 'filters', 'user', 'utilities', 'shared', 'database', 'decorators', 'premium']
