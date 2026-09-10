20e46280180f47f982f5d3a328f57e0d9272dc37
BreezeKun <akshadsatpute1989@gmail.com>
2026-09-10 19:10:38 +0530

    fixed init

--- ronova/plugins/user/__init__.py
+++ ronova/plugins/user/__init__.py
@@ -1,9 +1,11 @@
 from . import (ani_search, delete, eval, just_google_it,
                logs, search_movie, search_word, shell,
-               start, stats, think, wiki, afk)
+               start, stats, think, wiki, afk, xox, play, help,
+               sudo, quotely, )
 
 __all__ :list[str] = [
     "ani_search", "delete", "eval", "just_google_it",
     "logs", "search_movie", "search_word", "shell",
-    "start", "stats", "think", "wiki", "afk"
+    "start", "stats", "think", "wiki", "afk",
+    "xox", "play", "help", "sudo", "quotely"
 ]
