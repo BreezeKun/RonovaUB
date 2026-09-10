07245917d6dc77cb8a74d691505956ba22b5e769
BreezeKun <akshadsatpute1989@gmail.com>
2026-09-10 19:12:49 +0530

    fixed init

--- ronova/plugins/bot/__init__.py
+++ ronova/plugins/bot/__init__.py
@@ -2,12 +2,14 @@ from . import (
     inline_ani_search, inline_eval, inline_googleit,
     inline_logs, inline_movie_search, inline_pvt_message,
     inline_start, inline_stats, inline_think, inline_wiki,
-    inline_word_search, inline_rich, send_dm
+    inline_word_search, inline_rich, send_dm,
+    inline_card, inline_help, inline_prem, inline_xox
 )
 
 __all__: list[str] = [
     "inline_ani_search", "inline_eval", "inline_googleit",
     "inline_logs", "inline_movie_search", "inline_pvt_message",
     "inline_start", "inline_stats", "inline_think", "inline_wiki",
-    "inline_word_search", "inline_rich", "send_dm"
+    "inline_word_search", "inline_rich", "send_dm",
+    "inline_card", "inline_help", "inline_prem", "inline_xox"
 ]
