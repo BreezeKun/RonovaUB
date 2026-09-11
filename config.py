9d1c3ea239e296dfaa41f6c194064f51266cde5f
BreezeKun <akshadsatpute1989@gmail.com>
2026-09-11 12:55:32 +0530

    Added 2nd client

--- config.py
+++ config.py
@@ -6,12 +6,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID: int = int(os.getenv('api_id','0'))
-API_HASH: str = os.getenv('api_hash','')
+API_ID_C1: int = int(os.getenv('api_id_1','0'))
+API_HASH_C1: str = os.getenv('api_hash_1','')
+SESSION_STRING_C1: str = os.getenv('string_session_1','')
+API_ID_C2: int = int(os.getenv('api_id_2','0'))
+API_HASH_C2: str = os.getenv('api_hash_2','')
+SESSION_STRING_C2: str = os.getenv('string_session_2','')
 BOT_TOKEN: str = os.getenv('bot_token','')
 BOT:str = os.getenv('bot','')
 ADMIN_ID: list[int | str] = [int(os.getenv('admin','[0]'))]
-SESSION_STRING: str = os.getenv('string_session','')
 TAVILY_KEY: str = os.getenv('tavily_key','')
 TMDB_KEY:str = os.getenv('tmdb_key','')
 GEMINI_KEY:str = os.getenv('gemini_key','')
@@ -24,7 +27,8 @@ GI_UID:int = int((os.getenv('gi_uid','0')))
 PREFIXES: list[str] = [".", "@", "#", "$", "%", "^", "&", "*", "~", ""]
 
 __all__:list[str] = [
-    "API_ID", "API_HASH", "BOT_TOKEN",
-    "BOT", "ADMIN_ID", "SESSION_STRING",
+    "API_ID_C1", "API_HASH_C1", "BOT_TOKEN",
+    "API_ID_C2", "API_HASH_C2","SESSION_STRING_C2",
+    "BOT", "ADMIN_ID", "SESSION_STRING_C1",
     "TAVILY_KEY", "TMDB_KEY", "GEMINI_KEY",
     "GROQ_KEY", "POSTGRE_KEY"]
